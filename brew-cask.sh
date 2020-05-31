e6a087953e45cd1cd45bcb181ddaf843c287dfdf
Max Shypko <maximshipko@gmail.com>
2020-05-31 15:27:57 +0200

    chore: remove not used configurations and packages

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,56 +1,22 @@
-#!/bin/bash
+#!/bin/zsh
 
+# https://github.com/Homebrew/homebrew-cask/blob/master/USAGE.md
 
 # to maintain cask ....
-#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
+#     brew cleanup && brew update && brew cask upgrade`
 
-
-# Install native apps
-
-brew install caskroom/cask/brew-cask
-# brew tap caskroom/versions
 
 # daily
-brew cask install spectacle
-brew cask install dropbox
-brew cask install gyazo
-brew cask install 1password
-brew cask install rescuetime
-# brew cask install flux
-
-# dev
-brew cask install iterm2
 brew cask install sublime-text
-brew cask install imagealpha
-brew cask install imageoptim
-
-# fun
-brew cask install limechat
-# brew cask install miro-video-converter
-# brew cask install horndis               # usb tethering
-
-# browsers
-# brew cask install google-chrome-canary
-# brew cask install firefoxnightly
-# brew cask install webkit-nightly
-# brew cask install chromium
-# brew cask install torbrowser
-
-# less often
-brew cask install disk-inventory-x
-brew cask install screenflow
+brew cask install visual-studio-code
+brew cask install google-chrome
+brew cask install dropbox
 brew cask install vlc
-brew cask install gpgtools
-brew cask install licecap
-# brew cask install utorrent
-
+brew cask install skype
+#brew cask install skype-for-business
 brew cask install spotify
 
-brew tap caskroom/fonts 
+brew tap homebrew/cask-fonts
 brew cask install font-fira-code
 
 
-# Not on cask but I want regardless.
-
-# File Multi Tool 5
-# Phosphor
